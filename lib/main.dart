227f4ec9af8e8c877c229847fcdbb85566442bcf
Adrien Maranville <adrien@maranville.io>
2019-09-22 21:48:53 -0600

    added app functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int randId = getRandomNum();
+  static getRandomNum() {
+    return new Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[300],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[700],
+          title: Text(
+            'Ask Me Anything',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Center(
+                    child: FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          randId = getRandomNum();
+                        });
+                      },
+                      child: Image.asset("images/ball$randId.png"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
